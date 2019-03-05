87acd4aa3244272b40ca386dff0516ff02fbdcfd
zhangtongle <1071235258@qq.com>
2019-03-05 10:49:34 +0800

    this is hello

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+    }
+
+
+}
